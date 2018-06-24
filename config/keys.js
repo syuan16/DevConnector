9190b329a8c82b68861a7513f5bf10d1f0db6501
Stephen Yuan <stephenyuan@StephendeMacBook-Pro.local>
2018-06-24 10:54:49 -0500

    finished validation and Profile and User models

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://stephen:YWYwh0513@ds115931.mlab.com:15931/testdemo"
+  mongoURI: "mongodb://stephen:YWYwh0513@ds115931.mlab.com:15931/testdemo",
+  secretOrKey: "secret"
 };
